e2748fd76cd6c65c4bb138ba5d0b4c7c22072f3f
Rich Sage <rich.sage@gmail.com>
2017-02-16 10:57:36 +0000

    [FORKED] Apply drag & drop re-ordering patch.

--- src/Form/NodeorderAdminDisplayForm.php
+++ src/Form/NodeorderAdminDisplayForm.php
@@ -10,6 +10,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
 use Drupal\taxonomy\Entity\Term;
 
+
 /**
  * Implements an example form.
  */
@@ -26,27 +27,8 @@ class NodeorderAdminDisplayForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    global $pager_page_array, $pager_total, $pager_total_items;
-
-    $page            = isset($_GET['page']) ? $_GET['page'] : 0;
-    $page_increment  = \Drupal::config('nodeorder.settings')->get('taxonomy_terms_per_page_admin');  // Number of terms per page.
-    $page_entries    = 0;   // Elements shown on this page.
-    $before_entries  = 0;   // Elements at the root level before this page.
-    $after_entries   = 0;   // Elements at the root level after this page.
-    $root_entries    = 0;   // Elements at the root level on this page.
 
     $tid = \Drupal::request()->attributes->get('taxonomy_term');
-
-    $term = Term::load($tid);
-
-    // Build form tree.
-    $form = array(
-      '#tree' => TRUE,
-      '#parent_fields' => FALSE,
-      '#term' => $term,
-    );
-    $form['#title'] = $this->t('Order nodes for <em>%term_name</em>', array('%term_name' => $term->label()));
-
     $node_ids = db_select('taxonomy_index', 'ti')
       ->fields('ti', ['nid', 'weight'])
       ->condition('ti.tid', $tid)
@@ -54,37 +36,47 @@ class NodeorderAdminDisplayForm extends FormBase {
       ->execute()
       ->fetchAllKeyed();
     $nodes = Node::loadMultiple(array_keys($node_ids));
-    $node_count = count($nodes);
 
-    // Weights range from -delta to +delta, so delta should be at least half
-    // of the amount of blocks present. This makes sure all blocks in the same
-    // region get an unique weight.
-    $weight_delta = round($node_count / 2);
-    $current_weight = $node_ids;
+    $term = Term::load($tid);
 
-    foreach ($nodes as $node) {
-      $form[$node->id()]['#node'] = $node;
-      $form[$node->id()]['title'] = array(
-        '#markup' => $node->label());
-      $form[$node->id()]['weight'] = array(
+    $form['#title'] = $this->t('Order nodes for <em>%term_name</em>', array('%term_name' => $term->label()));
+    $form['#term_id'] = $tid;
+
+    $form['nodes'] = array(
+      '#type' => 'table',
+      '#header' => array(t('Title'), t('Weight')),
+      '#empty' => t('No nodes exist in this category.'),
+      '#tabledrag' => array(
+        array(
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'nodes-order-weight',
+        ),
+      ),
+    );
+
+    foreach ($nodes as $id => $entity) {
+      $form['nodes'][$id]['#attributes']['class'][] = 'draggable';
+      $form['nodes'][$id]['#weight'] = $entity->id();
+      $form['nodes'][$id]['label'] = array(
+        '#plain_text' => $entity->label(),
+      );
+
+
+      $form['nodes'][$id]['weight'] = array(
         '#type' => 'weight',
-        '#title' => $this->t('Weight for @title', array('@title' => $node->label())),
+        '#title' => t('Weight for @title', array('@title' => $entity->label())),
         '#title_display' => 'invisible',
-        '#delta' => $weight_delta,
-        '#default_value' => $current_weight[$node->id()],
+        '#default_value' => $entity->id(),
+        '#attributes' => array('class' => array('nodes-order-weight')),
       );
     }
-
-    if (empty($node_count)) {
-      $form['empty'] = array(
-        '#markup' => '<div>' . $this->t('No nodes exist in this category') . '</div>',
-      );
-    }
-
-    $form['submit'] = array(
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
       '#type' => 'submit',
       '#value' => t('Save order'),
     );
+
     return $form;
   }
 
@@ -101,9 +93,8 @@ class NodeorderAdminDisplayForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $tid = $form['#term']->tid->value;
-
-    foreach ($form_state->getValues() as $nid => $node) {
+    $tid = $form['#term_id'];
+    foreach ($form_state->getValue(array('nodes')) as $nid => $node) {
       // Only take form elements that are blocks.
       if (is_array($node) && array_key_exists('weight', $node)) {
         db_update('taxonomy_index')->fields(['weight' => $node['weight']])
